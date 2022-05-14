812cdd449c345ed9e4b0b17e56744d9c356be6d4
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-05-15 02:47:52 +0600

    add dark mode theme

--- src/Pages/Home/Home/Services/Services.js
+++ src/Pages/Home/Home/Services/Services.js
@@ -47,7 +47,7 @@ const Services = () => {
                         <img src={treatment} alt="Album" />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title text-4xl font-bold text-accent">Exceptional Dental Care, on Your Terms</h2>
+                        <h2 className="card-title text-4xl font-bold ">Exceptional Dental Care, on Your Terms</h2>
                         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                         <div className="card-actions justify-start">
                             <PrimaryButton>Get Started</PrimaryButton>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,31 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        mytheme: {
-          primary: "#19D3AE",
-          secondary: "#0FCFEC",
-          accent: "#3A4256",
-          neutral: "#3d4451",
+        light: {
+          "primary": "#19D3AE",
+          "secondary": "#0FCFEC",
+          "accent": "#3A4256",
+          "neutral": "#3d4451",
           "base-100": "#ffffff",
+          "info": "#98A8DD",
+          "success": "#1BBB70",
+          "warning": "#DF7E07",
+          "error": "#FA5C5C",
         },
       },
-      "dark",
+      {
+        dark: {
+          "primary": "#96B2E3",
+          "secondary": "#75EACF",
+          "accent": "#4B5563",
+          "neutral": "#F3F4F6",
+          "base-100": "#1F2937",
+          "info": "#98A8DD",
+          "success": "#1BBB70",
+          "warning": "#F59E0B",
+          "error": "#FB7185",
+        },
+      },
       "cupcake",
     ],
   },
